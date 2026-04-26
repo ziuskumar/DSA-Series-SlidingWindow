9e005cb5fb7a90a4365f9a59cd9ee53ecfe62ddd
zius kumar <153058154+ziuskumar@users.noreply.github.com>
2026-04-26 11:46:27 +0530

    Implement lengthOfLongestSubstring function

--- Revisiion.cpp
+++ Revisiion.cpp
@@ -22,6 +22,140 @@ public:
         return maxsum;
     }
 };
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int n = s.size();
+        int l=0, r=0, maxlen = 0;
+
+        int hash[256];
+        memset(hash, -1, sizeof(hash));
+
+        while(r < n){
+            if(hash[s[r]] != -1 && hash[s[r]] >= l){
+                l = hash[s[r]] + 1;
+            }
+
+            int len = r - l + 1;
+            maxlen = max(maxlen, len);
+
+            hash[s[r]] = r;
+            r++;
+        }
+
+        return maxlen;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         int n = s.size();
+//         int l = 0, r = 0;
+//         int hash[256];
+//         memset(hash, -1, sizeof(hash));
+        
+//         int maxlen = 0;
+
+//         while(r < n){
+
+//             if(hash[s[r]] != -1 && hash[s[r]] >= l){
+//                 l = hash[s[r]] + 1;
+//             }
+
+//             int len = r - l + 1;
+//             maxlen = max(maxlen, len);
+
+//             hash[s[r]] = r;
+//             r++;
+//         }
+//         return maxlen;
+        
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         int n = s.size();
+//         int hash[256];
+//         memset(hash , -1, sizeof(hash));
+//         int l = 0, r = 0, maxlen = 0;
+
+//         while(r < n){
+
+//             if(hash[s[r]] != -1 && hash[s[r]] >= l){
+//                 l = hash[s[r]] + 1;
+//             }
+
+//             int len = r - l + 1;
+//             maxlen = max(len , maxlen);
+
+//             hash[s[r]] = r;
+//             r++;
+//         }
+//         return maxlen;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         int n = s.size();
+//         int maxlen = 0;
+
+//         for(int i=0; i<n; i++){
+//             int hash[256] = {0};
+
+//             for(int j=i; j<n; j++){
+//                 if(hash[s[j]] == 1){
+//                     break;
+//                 }    
+
+//                 int len = j-i+1;
+//                 maxlen = max(maxlen, len);
+
+//                 hash[s[j]] = 1;
+                
+//             }
+//         }
+//         return maxlen;
+//     }
+// };
 
 
 
