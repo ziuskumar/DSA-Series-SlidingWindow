b4eaf768747e8d19f6d80447d75df57588bf7fce
zius kumar <153058154+ziuskumar@users.noreply.github.com>
2026-04-27 10:39:47 +0530

    Add totalFruit function to revsion
    Implement a solution to find the maximum number of fruits in two baskets.

--- Revisiion.cpp
+++ Revisiion.cpp
@@ -157,6 +157,40 @@ public:
 //     }
 // };
 
+Fruits into basket rev 
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        int n = fruits.size();
+        int l = 0, r = 0, maxlen = 0;
+        map<int,int>mpp;
+        
+        while(r < n){
+            mpp[fruits[r]]++;
+
+            while(mpp.size() > 2){
+                 
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]] == 0){
+                    mpp.erase(fruits[l]);
+                }
+                l++;
+            }
+
+            if(mpp.size() <= 2){
+                maxlen = max(maxlen, r - l + 1);
+            }
+            r++;
+        }
+
+        
+        return maxlen;
+    }
+};
+
+
+
 
 
 
